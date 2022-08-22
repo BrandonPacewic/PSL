26886ce2b59460be846129566bb64915c3839f29
Brandon <pacewicbrandon@gmail.com>
2022-08-22 08:32:01 -0700

    core cleanup

--- psl/inc/psl_yvals_core.h
+++ psl/inc/psl_yvals_core.h
@@ -7,20 +7,39 @@
 #ifndef PSL_YVALS_CORE_H_
 #define PSL_YVALS_CORE_H_
 
-#ifndef __cplusplus
 #define PSL_STRINGIZE_(S) #S
 #define PSL_STRINGIZE(S) PSL_STRINGIZE_(S)
+
+// PSL compiler checks
+#ifndef __cplusplus
 #pragma message(__FILE__ "(" PSL_STRINGIZE(__LINE__) "): PSL: Unexpected compiler, expected C++ compiler.")
-#error Error in C++ PSL usage
+#error Error in C++ PSL usage; Unsupported compiler.
 #endif  // __cplusplus
 
 // PSL requires C++20 or later.
 #if !(__cplusplus >= 201703L)
-#define PSL_STRINGIZE_(S) #S
-#define PSL_STRINGIZE(S) PSL_STRINGIZE_(S)
 #pragma message(__FILE__ "(" PSL_STRINGIZE(__LINE__) "): PSL: Unexpected compiler, expected C++20 compiler")
-#error Error in C++ PSL usage
-#endif
+#error Error in C++ PSL usage; Unsupported compiler.
+#endif // !(__cplusplus >= 201703L)
+
+// PSL os checks
+#if defined(_WIN32) || defined(_WIN64)
+#define PSL_OS_WINDOWS
+#if ENABLE_PSL_INTERNAL_DEBUG
+#pragma message(__FILE__ "(" PSL_STRINGIZE(__LINE__) "): PSL: Windows OS detected.")
+#endif // ENABLE_PSL_INTERNAL_DEBUG
+#elif defined(__linux__) || defined(__linux)
+#define PSL_OS_LINUX
+#if ENABLE_PSL_INTERNAL_DEBUG
+#pragma message(__FILE__ "(" PSL_STRINGIZE(__LINE__) "): PSL: Linux OS detected.")
+#endif // ENABLE_PSL_INTERNAL_DEBUG
+#elif defined(__APPLE__) || defined(__MACH__)
+#pragma message(__FILE__ "(" PSL_STRINGIZE(__LINE__) "): PSL: Mac OS detected. Unsupported OS.")
+#error Error in C++ PSL usage; Unsupported OS.
+#else
+#pragma message(__FILE__ "(" PSL_STRINGIZE(__LINE__) "): PSL: Unknown OS detected. Unsupported OS.")
+#error Error in C++ PSL usage; Unsupported OS.
+#endif // defined(_WIN32) || defined(_WIN64)
 
 #define PSL_VERSION_MAJOR 0
 #define PSL_VERSION_MINOR 1
@@ -38,5 +57,8 @@
 #else  // ^^^ ENABLE_PSL_INTERNAL_DEBUG ^^^ // vvv !ENABLE_PSL_INTERNAL_DEBUG vvv
 #define PSL_INTERNAL_STATIC_ASSERT(...)`
 #endif  // ENABLE_PSL_INTERNAL_DEBUG
+
+#undef PSL_STRINGIZE
+#undef ENABLE_PSL_INTERNAL_DEBUG
 
 #endif  // PSL_YVALS_CORE_H_
